f1d85ccd439652621e47125d373d45e30a1616cf
Arijit <subhasghosh196@outlook.com>
2022-08-19 16:46:39 +0530

    modified

--- HackerRank/problem solving/migratorybirds.py
+++ HackerRank/problem solving/migratorybirds.py
@@ -1,14 +1,20 @@
 def migratoryBirds(arr):
     # Write your code here
     maxElem=0
-    arr.sort()
-    for i in range(len(arr)):
-        countelem=arr.count(arr[i])
-        if(maxElem<countelem):
-             maxElem=countelem
-             val=arr[i]
+    # arr_id=0
+    # arr.sort()
+    countelem=[]
+    for i in range(5):
+        countelem.append(arr.count(i+1))
+    print(countelem)
+    for i in range(5):
+        if(maxElem<countelem[i]):
+             maxElem=countelem[i]
+             val=i+1
+            #  arr_id=i+1
+             
     return val
 
-# l=[1,3,3,3,4]
-l=[3,4,4,2,2,5]
+# l=[1,3,3,3,3,4]
+l=[3,4,4,2,2,5,5,5]
 print(migratoryBirds(l))
