1100e7693a79d6cecc39fb9401e0198bb44dbc54
Arijit <subhasghosh196@outlook.com>
2022-08-30 20:22:30 +0530

    added

---
+++ HackerRank/python/mutateString.py
@@ -0,0 +1,15 @@
+def mutate_string(string, position, character):
+    string=list(string)
+    string.pop(position)
+    string.insert(position,character)
+    string=''.join(string)
+    return string
+
+
+
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
