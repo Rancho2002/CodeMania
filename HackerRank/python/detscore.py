1df5124fe1cf9c04dd93b79f335f46a0c185acf4
Arijit <subhasghosh196@outlook.com>
2022-09-06 20:32:32 +0530

    added

---
+++ HackerRank/python/detscore.py
@@ -0,0 +1,10 @@
+# https://www.codechef.com/submit/DETSCORE
+# cook your dish here
+a=int(input())
+for i in range(a):
+    a,b=map(int,input().split())
+    e=a/10
+    s=b*e 
+    print(int(s))
+    
+    
