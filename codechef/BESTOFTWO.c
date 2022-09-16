e51f12785ed4e46c423bd09e2ffd6df5f4bbc0c7
Arijit <subhasghosh196@outlook.com>
2022-09-16 10:44:54 +0530

    added

---
+++ codechef/BESTOFTWO.c
@@ -0,0 +1,15 @@
+// https://www.codechef.com/submit/BESTOFTWO
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int total, a, b;
+	scanf("%d",&total);
+	for(int i=0;i<total;i++){
+	    scanf("%d%d",&a,&b);
+	    printf("%d\n",(a>=b)?a:b);
+	}
+	return 0;
+}
+
