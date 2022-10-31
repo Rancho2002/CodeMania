3e3d186a13a41ec019a7844c6eeac98b5585ca38
Arijit <subhasghosh196@outlook.com>
2022-10-31 13:22:47 +0530

    started LeetCode

---
+++ leetcode/Concatenation_of_Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        l=[]
+        for i in range(len(nums)):
+            l.append(nums[i])   
+        for i in range(len(nums)):
+            l.append(nums[i]) 
+        return l
+
+'''
+Runtime: 125 ms, faster than 76.08% of Python3 online submissions for Concatenation of Array.
+Memory Usage: 14.3 MB, less than 25.45% of Python3 online submissions for Concatenation of Array.
+'''
