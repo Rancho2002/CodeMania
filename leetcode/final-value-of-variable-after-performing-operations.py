92d6a65d03b15f4ffdf8eb1bb8cf36ebd9b797af
Arijit <subhasghosh196@outlook.com>
2022-11-03 10:54:52 +0530

    added alternative

--- leetcode/final-value-of-variable-after-performing-operations.py
+++ leetcode/final-value-of-variable-after-performing-operations.py
@@ -7,4 +7,16 @@ class Solution:
             else:
                 x-=1
         return x
+# Alternative
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        x=0
+        for i in operations:
+            if(i[1]=='+'): x+=1
+            else: x-=1
+        return x
             
