3e58c535abf7fbfae1d8f5d557811b795acfad99
Arijit <subhasghosh196@outlook.com>
2022-11-04 22:22:01 +0530

    dsa pattern 2

--- geeksforgeeks/square-pattern.py
+++ geeksforgeeks/square-pattern.py
@@ -5,3 +5,4 @@ class Solution:
             for j in range(N):
                 print("* ",end="")
             print()
+# https://practice.geeksforgeeks.org/problems/square-pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_1
