19feeff00df32319a12a4d86b8e11fbf0a4d2479
Arijit <subhasghosh196@outlook.com>
2022-12-03 23:57:16 +0530

    for better reminder&understanding

--- HackerRank/python/text-wrap.py
+++ HackerRank/python/text-wrap.py
@@ -1,4 +1,8 @@
 import textwrap
 
 def wrap(string, max_width):
+    print(textwrap.wrap(string,7))
     return textwrap.fill(string,max_width)
+
+
+print(wrap("hi arijit bhai kemon acho? SunlamSunechi valo acho. anondo kor",7))
