71b8818311c7c660a4e9bd6657cce62753a0c060
Arijit <subhasghosh196@outlook.com>
2022-12-13 01:35:08 +0530

    committed till 1 WA

--- newtonSchool/fearBallon.py
+++ newtonSchool/fearBallon.py
@@ -1,37 +1,15 @@
-a,b=input().split()
-l=list(map(int,input().split()))
-l.sort(reverse=True)
-# l= [-40, -10, 5, 20, 80]
-# l.remove(-40)
-newL=[]
-diff = lambda l: abs(l-0)
-for i in range(int(b)):
-    res=min(l,key=diff)
-    newL.append(res)
-    l.remove(res)
-# print(newL)
-distance=0
-removeE=[]
-for j in range(len(newL)):
-    # print(newL[j])
-    if(newL[j]<0):
-        # print(newL[j])
-        distance+=abs(newL[j]*2)
-        removeE.append(newL[j])
-
-for m in range(len(removeE)):
-    newL.remove(removeE[m])
-
-# print(distance)
-# print(newL)
-
-distance+=newL[0]
-for k in range(len(newL)-1):
-        # 6 + 1
-        # 7 + (2-1)
-        # 8 + (3-2)
-        # 9 + (4-3)
-        # 10
-    distance+=newL[k+1]-newL[k]
-print(distance)
-
+a,b=map(int,input().split())
+arr=list(map(int,input().split()))
+if(a==1):
+    print(arr[0])
+    exit
+h,l,test=b-1,0,[]
+while(h<a): # (number of ballon to burst-1) is less than have ballons
+    s=arr[h]-arr[l]
+    if(arr[h]<0 and arr[l]<0): s+=abs(arr[h])
+    elif(arr[l]>arr[h]): s+=abs(arr[h])
+    else: s+=abs(arr[l])
+    test.append(s)
+    h+=1
+    l+=1
+print(min(test))
