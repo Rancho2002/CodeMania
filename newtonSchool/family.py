efbb86f88fd4efa9cb516295e6b50e818528fd41
Arijit <subhasghosh196@outlook.com>
2022-12-13 17:16:44 +0530

    all AC

--- newtonSchool/family.py
+++ newtonSchool/family.py
@@ -1,6 +1,41 @@
 # https://my.newtonschool.co/playground/code/ivfg9mx69lbk
 # Your code here
-family=int(input())
+# family=int(input())
+# l=list(map(int,input().split()))
+# for i in range(1,family+1):
+#     print(l.count(i))
+
+# family=int(input())
+# N={}
+# for i in range(1,family+1):
+    # N[i]=0
+
+# l=list(map(int,input().split()))
+
+# for j in l:
+#     if(j==N.keys):
+#         N[j]+=1
+# print(N.keys(0))
+
+# Your code here
+n=int(input())
 l=list(map(int,input().split()))
-for i in range(1,family+1):
-    print(l.count(i))
+d ={}
+for i in l:
+    if i in d :
+        d[i] = d[i]+1
+        # print(d,"call hua1")
+    else:
+        d[i]=1
+        # print(d,"call hua2")
+
+# print(d)
+
+for i in range(1,n+1):
+    if i in d:
+        pass
+    else :
+        d[i]=0
+        
+for i in range(1,n+1):
+    print(d[i])
