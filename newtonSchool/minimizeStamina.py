9ced9b936eee4ce8c9c18bc5e16af1bd9ebdadc2
arijit ghosh <subhasghosh196@outlook.com>
2023-03-28 22:39:26 +0530

    more optimized brute force

--- newtonSchool/minimizeStamina.py
+++ newtonSchool/minimizeStamina.py
@@ -1,13 +1,12 @@
 n=int(input())
 l=list(map(int,input().split()))
 temp=0
-for i in range(1,101):
+for i in range(min(l),max(l)):
     temp=0
     for j in range(n):
         temp=temp+(i-l[j])**2
-    if(i==1 or temp<comp):    
+    if(i==min(l) or temp<comp):    
         comp=temp
         
 print(comp)
-
 # https://my.newtonschool.co/playground/code/z5r4ilbk329a
