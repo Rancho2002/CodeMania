0847327791078041cacfa5b4c575e886ec0a142f
arijit ghosh <subhasghosh196@outlook.com>
2023-07-13 03:51:06 +0530

    done

---
+++ interviewbit/RemoveElementfromArray.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def removeElement(self, A, B):
+        j=0
+        i=0       
+        while(i<len(A)):
+            if A[i]!=B:
+                A[i],A[j]=A[j],A[i]
+                j+=1
+            i+=1
+            
+        return j
